53a31c9d2a3a237569b14642a321f5d47df482fc
iuculano <82924525+iuculano@users.noreply.github.com>
2022-10-29 01:52:55 +0000

    Fix some things

--- src/zig-lang/install.sh
+++ src/zig-lang/install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -e
 
-
+VERSION=master
 if [[ $(dpkg --print-architecture) = 'amd64' ]]; then 
     architecture='x86_64-linux'
 else
     architecture='aarch64-linux'
 fi
 
-download_url=$(curl https://ziglang.org/download/index.json | jq ".${VERSION}[\"${architecture}\"].tarball")
+download_url=$(curl https://ziglang.org/download/index.json | jq -r ".${VERSION}[\"${architecture}\"].tarball")
 curl $download_url -o zig.tar.xz
 
 sudo mkdir /opt/zig-lang
-tar -xvf zig.tar.xz -C /opt/zig-lang
-sudo ln -s /opt/zig-lang/zig /usr/bin/
+sudo tar -xvf zig.tar.xz -C /opt/zig-lang
+sudo ln -s /opt/zig-lang/*/zig /usr/bin/
